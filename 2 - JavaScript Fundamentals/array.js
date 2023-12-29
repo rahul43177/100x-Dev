1b3cc0740da18cb755cfd345bb6ff67daedbccfa
Rahul Mishra <rahulmishra4317@gmail.com>
2023-12-29 10:17:26 +0000

    karde push

---
+++ 2 - JavaScript Fundamentals/array.js
@@ -0,0 +1,10 @@
+// const person1 = 'Rahul' ;
+// const person2 = 'Rohan';
+
+const persons = ['Rahul' , 'Rohan'];
+
+const person1 = persons[0];
+const person2 = persons[1];
+
+console.log(`Person 1 is ${person1} and person2 is ${person2}`);
+
--- 2 - JavaScript Fundamentals/programs.js
+++ 2 - JavaScript Fundamentals/programs.js
@@ -1,5 +1,13 @@
-let firstName = 'Rahul Mishra';
-let age = 23;
-let isMarried = false;
+// let firstName = 'Rahul Mishra';
+// let age = 23;
+// let isMarried = false;
 
-console.log("This persion is " + firstName + " and he is " + age + " years old " + 'and he is ' +(isMarried ? 'married' : 'not married'));
+// console.log("This persion is " + firstName + " and he is " + age + " years old " + 'and he is ' +(isMarried ? 'married' : 'not married'));
+
+//for loop - finding the sum till 100
+let answer = 0;
+for(let i = 1;i<=100;i++) {
+    answer = answer + i;
+}
+
+console.log(answer);
