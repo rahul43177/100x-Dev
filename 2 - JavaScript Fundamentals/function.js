41b4a93fdd228d4d89241ee572fa060fbb4a92c2
Rahul <rahulmishra4317@gmail.com>
2023-12-29 16:09:21 +0530

    functions

---
+++ 2 - JavaScript Fundamentals/function.js
@@ -0,0 +1,12 @@
+function sumTwoNumbers (num1 , num2) {
+    const sum = num1+num2;
+    return sum;
+}
+
+
+const sum1 = sumTwoNumbers(1,2)
+const sum2 = sumTwoNumbers(2,3)
+const sum3 = sumTwoNumbers(5,3)
+const sum4 = sumTwoNumbers(5,2)
+
+console.log(sum1,sum2,sum3,sum4)
--- 2 - JavaScript Fundamentals/programs.js
+++ 2 - JavaScript Fundamentals/programs.js
@@ -1,10 +1,10 @@
-// let firstName = 'Rahul Mishra';
-// let age = 23;
-// let isMarried = false;
+let firstName = 'Rahul Mishra';
+let age = 23;
+let isMarried = false;
 
-// console.log("This persion is " + firstName + " and he is " + age + " years old " + 'and he is ' +(isMarried ? 'married' : 'not married'));
+console.log("This persion is " + firstName + " and he is " + age + " years old " + 'and he is ' +(isMarried ? 'married' : 'not married'));
 
-//for loop - finding the sum till 100
+// for loop - finding the sum till 100
 let answer = 0;
 for(let i = 1;i<=100;i++) {
     answer = answer + i;
