afe85c1a0e72e2fde8038a924fb7edb57b44e837
Rahul Mishra <rahulmishra4317@gmail.com>
2023-12-29 10:04:33 +0000

    program to greet a person

---
+++ 2 - JavaScript Fundamentals/programs.js
@@ -0,0 +1,5 @@
+let firstName = 'Rahul Mishra';
+let age = 23;
+let isMarried = false;
+
+console.log("This persion is " + firstName + " and he is " + age + " years old " + 'and he is ' +(isMarried ? 'married' : 'not married'));
