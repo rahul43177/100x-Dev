f3b7c3c8121d4d30696e1292d00ec933a7375316
Rahul <rahulmishra4317@gmail.com>
2024-01-03 22:45:51 +0530

    JavaScript fundamentals are done

---
+++ 2 - JavaScript Fundamentals/callbackFunction.js
@@ -0,0 +1,55 @@
+// function sqaure(n) {
+//     return n*n;
+// }
+// function cube(a) {
+//     return a*a*a;
+// }
+// function sumOfSqaures(a,b) { //in this function we called sqaure which is another function , this is not callback function
+//     const val1 = sqaure(a);
+//     const val2 = sqaure(b);
+
+//     return val1+val2;
+// }
+// function sumOfCubes(a,b) {
+//     const val1 = cube(a)
+//     const val2 = cube(b);
+//     return val1+val2;
+// }
+// const ans = sumOfSqaures(2,3)
+// console.log(ans)
+
+// const sumCube = sumOfCubes(2,2)
+// console.log(sumCube)
+// //2 square - 4
+// //3 square - 9
+// // sum - 13
+
+// sum function 
+// another which will just log things out 
+
+// function printing(a) {
+//     console.log(a);
+// }
+
+// function sum(a,b,fn) {
+//     const sum = a+b;
+//     fn(sum);
+// }
+
+// sum(2,4,printing);
+
+
+// function doubleIt(number) {
+//     return number*2;
+// }
+
+// function addTwoNumberAndDoubleIt(num1 , num2) {
+//     const two = num1+num2;
+//     const double = doubleIt(two)
+
+//     return double;
+// }
+
+// console.log(addTwoNumberAndDoubleIt(2,3))
+
+
