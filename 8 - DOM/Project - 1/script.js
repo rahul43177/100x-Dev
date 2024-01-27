81d8ea13dcadc48df1228892ac6d5428c3f5553a
rahul43177 <rahulmishra4317@gmail.com>
2024-01-27 22:00:11 +0000

    This is from replit

--- 8 - DOM/Project - 1/script.js
+++ 8 - DOM/Project - 1/script.js
@@ -1,13 +1,16 @@
-function populateDiv() {
-  const element = document.getElementById("finalSum");
+function findSum() {
+  const num1 = document.querySelector("#firstNumber")
+  console.log(num1)
+  console.log(num1.value)
+  console.log(`number 2 ---> ${num2} and then value ${num2.value}  `)
+  const num2 = document.querySelector("#secondNumber")
+  const result = document.quertSelector("#result")
 
-  const num1 = parseInt(document.getElementById("firstNumber").value);
-  const num2 = parseInt(document.getElementById("secondNumber").value);
-
-  if(isNaN(num1) && isNaN(num2)) {
-    element.innerHTML = 'Please enter two valid numbers'
-  }else {
-    element.innerHTML = num1 + num2;
+  if(isNan(num1) && isNaN(num2)) {
+    result.innerHTML = "Please enter the valid input numbers , both are invalid"  
+  } else if(isNaN(num1) || isNaN(num2)) {
+    result.innerHTML = "Please enter the valid input numbers , one of them  is invalid";
+  } else {
+    result.innerHTML = `The sum of ${num1} and ${num2} is --> ${num1+num2}`
   }
-  
 }
