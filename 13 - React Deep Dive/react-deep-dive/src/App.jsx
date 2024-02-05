9c9181abba86c7facb71188e330c66d95962b3c7
Rahul <rahulmishra4317@gmail.com>
2024-02-05 18:53:41 +0530

    add todos

--- 13 - React Deep Dive/react-deep-dive/src/App.jsx
+++ 13 - React Deep Dive/react-deep-dive/src/App.jsx
@@ -1,25 +1,55 @@
-import {useState , memo} from 'react'
+import {useState} from 'react'
 
 function App() {
-  const [title , setTitle] = useState("My name is khan")
-  function updateTitle() {
-    setTitle(`My name is ${Math.random()}`)
+  let todosArray = [
+    {
+      id : 1 , 
+      title : "Go to gym" ,
+      description : " Do biceps" 
+    } , 
+    {
+      id : 2 , 
+      title : "study DSA" , 
+      description : "complete arrays" ,
+    } ,
+    {
+      id : 3 ,
+      title : "study React " ,
+      description : "study the class"
+    }
+  ]
+  const [todos , setTodos] = useState(todosArray)
+  
+  function addTodos() {
+    setTodos([...todos , {
+      id : 4 , 
+      title : Math.random() , 
+      description : Math.random()
+    }])
   }
+
+
   return (
     <div>
-      <button onClick={updateTitle}>Click here to update the title</button>
-      <Header title = "HAHAHAHA"/>
-      <Header title = "rasad"/>
-      <Header title = {title}/>
+      <button onClick={addTodos} >Add a todo</button> 
+    {todos.map((todo)=> {
+      return <Todo title = {todo.title} description={todo.description} />
+    })}
     </div>
   )
+
 }
 
-const Header = memo(function Header({title}) {
+
+function Todo({title , description}) {
   return <div>
+
+  <h1>
     {title}
+  </h1>
+  <h5>{description}</h5>
   </div>
-} )
+}
 
 
 export default App
