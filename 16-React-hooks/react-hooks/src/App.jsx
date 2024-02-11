7d4b94add0f793cc1d8da3a30fa9bd12741ebcf2
Rahul <rahulmishra4317@gmail.com>
2024-02-11 23:23:37 +0530

    working now

--- 16-React-hooks/react-hooks/src/App.jsx
+++ 16-React-hooks/react-hooks/src/App.jsx
@@ -1,29 +1,30 @@
-import {useState , useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div>
-    <Todo/>
-  </div>
+  return (
+    <div>
+      <Todo id={3} />
+    </div>
+  );
 }
 
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
+  useEffect(() => {
+    axios
+      .get(`https://sum-server.100xdevs.com/todos?id=${id}`)
+      .then((response) => {
+        setTodo(response.data.todos[0]);
+      });
+  }, []);
 
-function Todo() {
-  const [todo , setTodo] = useState({})
-
-  useEffect(()=> {
-    axios.get(`https://sum-server.100xdevs.com/todos`)
-    .then((response)=> {
-      setTodo(response.data.todos)
-    })
-  },[])
-  return <div>
-    <h1>{todo.title}</h1>
-    <h2>{todo.description}</h2>
-  </div>
+  return (
+    <div>
+      <h3>{todo.title}</h3>
+      <h5>{todo.description}</h5>
+    </div>
+  );
 }
 
-
-
-
-export default App
+export default App;
