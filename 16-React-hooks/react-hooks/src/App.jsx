20a198e525aa8df6fe605221daff35909ba4a9d5
Rahul <rahulmishra4317@gmail.com>
2024-02-12 23:28:06 +0530

    coded tutoral

--- 16-React-hooks/react-hooks/src/App.jsx
+++ 16-React-hooks/react-hooks/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [selectedId, setSelectedId] = useState(1);
-  return (
-    <div>
-      <button onClick={() => setSelectedId(1)}>1</button>
-      <button onClick={() => setSelectedId(2)}>2</button>
-      <button onClick={() => setSelectedId(3)}>3</button>
-      <button onClick={() => setSelectedId(4)}>4</button>
+  const [counter, setCounter] = useState(0);
+  const [intputValue, setInputValue] = useState(1);
 
-      {`Selected ID : ${selectedId}`}
-      <Todo id={selectedId} />
-    </div>
-  );
-}
-
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-  useEffect(() => {
-    axios
-      .get(`https://sum-server.100xdevs.com/todos?id=${id}`)
-      .then((response) => {
-        setTodo(response.data.todos[0]);
-      });
-  }, [id]);
+  let sum = 0;
+  for (let i = 1; i <= intputValue; i++) {
+    sum += i;
+  }
 
   return (
     <div>
-      <h3>{todo.title}</h3>
-      <h5>{todo.description}</h5>
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        type="text"
+        placeholder="Enter a number to find sum till n"
+      />
+      <br /> <br />
+      The sum from 1 to {intputValue} is {sum}
+      <br />
+      <br />
+      <button onClick={() => setCounter(counter + 1)}>
+        Press to increase the counter
+      </button>
+      {counter}
     </div>
   );
 }
