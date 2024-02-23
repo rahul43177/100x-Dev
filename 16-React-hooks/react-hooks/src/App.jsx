386ee2d2cabec21e3b6a110a33a868130c1ede4e
Rahul <rahulmishra4317@gmail.com>
2024-02-23 14:06:11 +0530

    use effect

--- 16-React-hooks/react-hooks/src/App.jsx
+++ 16-React-hooks/react-hooks/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
-  const [selectedButton , setSelectedButton] = useState(0)
+  const [selectedButton, setSelectedButton] = useState(1);
   return (
     <>
-      <button onClick={() => setSelectedButton(1)} >1</button>
-      <button onClick={() => setSelectedButton(2)} >2</button>
-      <button onClick={() => setSelectedButton(3)} >3</button>
-      <button onClick={() => setSelectedButton(4)} >4</button>
+      <button onClick={() => setSelectedButton(1)}>1</button>
+      <button onClick={() => setSelectedButton(2)}>2</button>
+      <button onClick={() => setSelectedButton(3)}>3</button>
+      <button onClick={() => setSelectedButton(4)}>4</button>
+      {console.log(`selected button --> ${selectedButton}`)}
+      <Todo id={selectedButton} />
     </>
   );
 }
 
 function Todo({ id }) {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState({});
   useEffect(() => {
     axios
       .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
@@ -23,7 +25,8 @@ function Todo({ id }) {
         console.log("response.data.todo", response.data.todo);
         setTodo(response.data.todo);
       });
-  }, []);
+  }, [id]); //here whenever id changed , we need to make the backend call
+  //dependency array 
 
   return (
     <div>
