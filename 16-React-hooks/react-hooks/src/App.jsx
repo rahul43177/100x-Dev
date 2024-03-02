f3c3ca29ae7e33482ff88683d811d22eed4fbaaa
Rahul <rahulmishra4317@gmail.com>
2024-03-02 14:25:18 +0530

    react hooks and custom hooks learnt

--- 16-React-hooks/react-hooks/src/App.jsx
+++ 16-React-hooks/react-hooks/src/App.jsx
@@ -1,39 +1,25 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import {useState , useEffect} from 'react'
+
+//this is custom hook called - useTodos
+function useTodos() {
+  const [todos , setTodos] = useState([])
+  useEffect(()=> {
+    axios.get("")
+    .then((res)=> {
+      setTodos(res.data.todos)
+    })
+  },[])
+  return todos;
+}
+
 function App() {
-  const [selectedButton, setSelectedButton] = useState(1);
-  return (
-    <>
-      <button onClick={() => setSelectedButton(1)}>1</button>
-      <button onClick={() => setSelectedButton(2)}>2</button>
-      <button onClick={() => setSelectedButton(3)}>3</button>
-      <button onClick={() => setSelectedButton(4)}>4</button>
-      {console.log(`selected button --> ${selectedButton}`)}
-      <Todo id={selectedButton} />
-    </>
-  );
-}
-
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-  useEffect(() => {
-    axios
-      .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-      .then((response) => {
-        console.log("response", response);
-        console.log(` data.data ${response.data.todo}`);
-        console.log("response.data.todo", response.data.todo);
-        setTodo(response.data.todo);
-      });
-  }, [id]); //here whenever id changed , we need to make the backend call
-  //dependency array 
+  const todos = useTodos();
 
   return (
-    <div>
-      <h1>{todo.title}</h1>
-      <h4>{todo.description}</h4>
-    </div>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
   );
 }
-
-export default App;
