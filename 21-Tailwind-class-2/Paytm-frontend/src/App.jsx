08801b9ebd96ecfc50a65b7364f02343bbc88d9e
Rahul <rahulmishra4317@gmail.com>
2024-05-12 13:02:11 +0530

    tailwind

---
+++ 21-Tailwind-class-2/Paytm-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        Hi there
+      </div>
+    </>
+  )
+}
--- 21-Tailwind-class-2/Tailwind/src/App.jsx
+++ 21-Tailwind-class-2/Tailwind/src/App.jsx
@@ -2,14 +2,27 @@
 
 export default function App() {
   return (
-    <div className="grid grid-cols-3">
-      <div className="bg-red-500 col-span-4">Hi</div>
-      <div className="bg-yellow-500 col-span-4">My</div>
-      <div className="bg-green-500 col-span-2">Name</div>
-      <div className="bg-blue-500">is</div>
-      <div className="text-blue-400 col-span-5">Rahul</div>
-      <div className="bg-indigo-500">hahaha</div>
+    //grid
+    // <div className="grid grid-cols-3 ">
+    //   <div className="bg-red-500 col-span-4">Hi</div>
+    //   <div className="bg-yellow-500 col-span-4">My</div>
+    //   <div className="bg-green-500 col-span-2">Name</div>
+    //   <div className="bg-blue-500">is</div>
+    //   <div className="text-blue-400 col-span-5">Rahul</div>
+      // <div className="bg-indigo-500">hahaha</div>
+    //</div>
+    //responsiveness
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+      <div className="bg-red-500">Hello</div>
+      <div className="bg-blue-500">Good</div>
+      <div className="bg-blue-500">Good</div>
+      <div className="bg-blue-500">Good</div>
+      <div className="bg-blue-500">Good</div>
+      <div className="bg-blue-500">Good</div>
+      <div className="bg-blue-500">Good</div>
 
     </div>
+
+
   )
 }
