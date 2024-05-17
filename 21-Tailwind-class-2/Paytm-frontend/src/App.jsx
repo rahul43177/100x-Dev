65cdd224001a084fd13eb7dcdb145ffd148f17b5
Rahul <rahulmishra4317@gmail.com>
2024-05-17 10:54:12 +0530

    Payment app setup

--- 21-Tailwind-class-2/Paytm-frontend/src/App.jsx
+++ 21-Tailwind-class-2/Paytm-frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import './index.css'
 
 export default function App() {
   return (
-    <div className="bg-orange-500">
-      Hi there from APP jsx 
+    <div className="grid grid-cols-3">
+      <div>Hi there</div>
+      <div>Hi there</div>
+      <div>Hi there</div>
+      <div>Hi there</div>
+      <div>Hi there</div>
+      <div>Hi there</div>
     </div>
   )
 }
+
---
+++ 22-PaymentApp/src/App.jsx
@@ -0,0 +1,11 @@
+import { RevenueCard } from "./components/RevenueCard";
+
+
+export default function App() {
+
+  return (
+    <>
+      <RevenueCard title={"Amount Pending"} amount={"92929292"} orderCount={12}/>
+    </>
+  )
+}
