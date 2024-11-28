46dc4fded743136696a018a8b284c2caa5522762
Rahul <rahulmishra4317@gmail.com>
2024-11-28 17:17:36 +0530

    todo basic - class complete

---
+++ 10-BasicTodo-React/backend/controller.js
@@ -0,0 +1,3 @@
+module.exports.getAllTodos = async (req , res) => {
+    
+}
--- 10-BasicTodo-React/backend/index.js
+++ 10-BasicTodo-React/backend/index.js
@@ -1,11 +1,30 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
 
-app.listen(PORT , () => {
-    console.log(`The server is running on port ${PORT}`)
-})
+require("dotenv").config();
 
+const mongoDbURI = process.env.MONGO_DB_STRING;
+console.log("The uri --", mongoDbURI);
+
+const connect = async (uri) => {
+  console.log("Inside uri", uri);
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("MongoDB is connected");
+    })
+    .catch((e) => {
+      console.log("The error is :", e);
+    });
+};
+
+connect(mongoDbURI);
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
