569210e293a407a7220f98f9f6f3ed0f410084fa
Rahul <rahulmishra4317@gmail.com>
2024-12-01 23:23:21 +0530

    deleted node modules

--- 10-BasicTodo-React/backend/index.js
+++ 10-BasicTodo-React/backend/index.js
@@ -1,30 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+const express = require("express")
+const app = express()
+const PORT = 3000
+app.use(express.json())
 
-const PORT = 3000;
-
-app.use(express.json());
-
-require("dotenv").config();
-
-const mongoDbURI = process.env.MONGO_DB_STRING;
-console.log("The uri --", mongoDbURI);
-
-const connect = async (uri) => {
-  console.log("Inside uri", uri);
-  mongoose
-    .connect(uri)
-    .then(() => {
-      console.log("MongoDB is connected");
-    })
-    .catch((e) => {
-      console.log("The error is :", e);
-    });
-};
-
-connect(mongoDbURI);
-
-app.listen(PORT, () => {
-  console.log(`The server is running on port ${PORT}`);
-});
+app.listen(PORT , () => {
+  console.log("The server is running on " + PORT)
+})
