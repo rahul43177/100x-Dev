15d81eb433afcf50931d49b9c153f9916eb8796d
Rahul <rahulmishra4317@gmail.com>
2024-12-02 16:18:20 +0530

    Integration

--- 10-BasicTodo-React/frontend/components/Todos.jsx
+++ 10-BasicTodo-React/frontend/components/Todos.jsx
@@ -14,21 +14,45 @@
 //     );
 // }
 
-export const Todos = ({todos}) => {
+// [
+//     {
+//         "_id": "674d794672ef1e3aa633cc4a",
+//         "title": "Complete todo application project",
+//         "description": "Complete the basics of react and todo application",
+//         "__v": 0
+//     },
+//     {
+//         "_id": "674d7a0f72ef1e3aa633cc54",
+//         "title": "Go for a walk",
+//         "description": "complete 10k in a walk",
+//         "__v": 0
+//     }
+// ]
+// export function Todos({ todos }) {
+//     console.log("inside todo component", todos);
+//     return (
+//         <>
+//             {todos.map(({ _id, title, description }) => {
+//                 console.log("Inside the todo", title);
+//                 return (
+//                     <div key={_id}>
+//                         <h3>{title}</h3>
+//                         <h4>{description}</h4>
+//                     </div>
+//                 );
+//             })}
+//         </>
+//     );
+// }
+export function Todos({todos}) {
+    console.log("The todos in todo component" , todos)
     return (
-        <>  
-            {todos.map(({id , title, description , isCompleted}) => {
+        <>
+            {todos.map(({todos})=> {
                 return (
-                    <div key = {id}>
-                        <h1>
-                            {title}
-                        </h1>
-                        <h3>
-                            {description}
-                        </h3>
-                        <button>
-                            {isCompleted ? "Mark as not completed" : "Mark as completed!"}
-                        </button>
+                    <div key = {todos._id}> 
+                        <h3>{todos.title}</h3>
+                        <h4>{todos.description}</h4>
                     </div>
                 )
             })}
--- 10-BasicTodo-React/frontend/src/App.jsx
+++ 10-BasicTodo-React/frontend/src/App.jsx
@@ -1,43 +1,33 @@
 import { CreateTodo } from "../components/CreateTodo";
 import { Todos } from "../components/Todos";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import axios from "axios";
 
 export default function App() {
+  const [todos , setTodos ] = useState([])
+
+  const fetchAllTheTodos = async () => {
+    const allTodosData = axios.get("http://localhost:3000/getTodo") 
+    .then((response) => {
+      if(response.data.status) {
+        setTodos(response.data.data)
+      }
+    })
+
+  }
+  useEffect(() => {
+    fetchAllTheTodos()
+  } , [])
   // const todosData = axios.get("http://localhost:3000/getTodo")
-  // console.log("The todoData API" , todosData)
+  // //console.log("The todoData API" , todosData)
   // const [todos , setTodos ] = useState([])
-  const todoDeatils = [
-    {
-      id : 1  ,
-      title: "First todo",
-      description: "This is the description of first todo",
-      isCompleted: true,
-    },
-    {
-      id : 2 , 
-      title: "Second Todo",
-      description: "This desc of second todo",
-      isCompleted: false,
-    },
-    {
-      id : 3 , 
-      title: "Third Todo",
-      description: "This desc of third todo",
-      isCompleted: false,
-    },
-    {
-      id : 4 , 
-      title: "Fourth Todo",
-      description: "This desc of fourth todo",
-      isCompleted: false,
-    },
-  ];
+  // const todoDeatils = ;
+  //console.log("Todos in useState" , todos)
   return (
     <div>
       Hi There! This is coming from App.jsx
       <CreateTodo />
-      <Todos todos={todoDeatils} />
+      <Todos todos = {todos} />
     </div>
   );
 }
