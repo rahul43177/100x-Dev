60ada65d658d853e091ca2c5d25251c1ed57d718
Rahul <rahulmishra4317@gmail.com>
2024-12-03 10:59:05 +0530

    neovim

--- 10-BasicTodo-React/backend/index.js
+++ 10-BasicTodo-React/backend/index.js
@@ -1,16 +1,14 @@
-const express = require("express")
-const router = require("./routes")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const router = require("./routes");
+const app = express();
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.use("/", router);
 
+app.listen(3000, () => {
+  console.log("The server is running on PORT --:3000");
+});
 
-app.use("/" , router)
-
-
-app.listen(3000 , () => {
-  console.log("The server is running on PORT --:3000")
-})
