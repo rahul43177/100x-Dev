5c5656fd6ee25956661186aed1525d71102302ff
Rahul <rahulmishra4317@gmail.com>
2024-12-10 02:25:43 +0530

    wrapper - all in one = react.memo

--- 12-TODOApplication/Todo Application/src/App.jsx
+++ 12-TODOApplication/Todo Application/src/App.jsx
@@ -1,47 +1,82 @@
-
-
+import {memo , useState} from 'react'
 
 export default function App() {
-    return (
-    <>
-        <Wrapper>
-            <h1>Hello World!</h1>
-            <p>This is a paragraph</p>
-            <WrapperOne>
-                This is wrapper inside wrapper
-            </WrapperOne>
-        </Wrapper>
-        <WrapperOne>
-            <h1>Hello World!</h1>
-            <p>This is a paragraph</p>
-        </WrapperOne>
-    </>
+    const [number , setNumber] = useState(0) ;
+    const names = ["Rahul Mishra" , "Muskan Acharya" , "Anshul Kumar" , "Ayush" , "Jena"];
 
-    )
-}
+    const randomName = names[Math.floor(Math.random()*names.length)]
 
-const WrapperOne = (props) => {
+    console.log("Name ---" , randomName);
+    const updateNumber = () => { 
+        setNumber(Math.floor(Math.random()*100))
+    }
     return (
-        <div style={{
-            display : "flex" , 
-            border : "2px solid green" , 
-            padding : "10px" , 
-            margin : 10
-        }}>
-            {props.children}
+        <div 
+            style = {{
+                    display : "flex" ,
+                    justifyContent : "space-around" ,
+                    alighItems : "space-around"
+            }}
+        >
+            <h1
+                style ={{
+                    color : "yellow" , 
+                    padding : 10 , 
+                    margin : 10 , 
+                    textAlign : "center" , 
+                    backgroundColor : "black" , 
+                    width : "150px" , 
+                }}
+            >
+                {randomName}
+            </h1>
+            <button 
+                onClick= {updateNumber}
+            >
+                Update the number!!!
+            </button>
+            <NumberComponent number = {number}/>
+            <NumberComponent number = {23}/>
+            <NumberComponent number = {33}/>
+            <NumberComponent number = {400}/>
+            <WrapperComponent>
+                <NumberComponent number = {number} />
+            </WrapperComponent>
         </div>
     )
 }
 
-export const Wrapper = (props) => {
-    return (
-        <div style={{
-            border : "2px solid black" , 
-            padding : 10 , 
-            margin : 10
-        }}>
-            {props.children}
-        </div>
-    )
-}
 
+const NumberComponent = memo(function NumberComponent({number}){
+
+    return (
+        <>
+            <div
+                style = {{
+                    display : "flex" , 
+                    justifyContent : "center" ,
+                    alignItems : "center" ,
+                    border : "2px solid lightblue" , 
+                    width : "60px" , 
+                    margin : 10,
+                    color : "red" ,
+                    fontWeight : "bold"
+                }}
+                >
+                Number : {number} 
+            </div>
+        </>
+    )
+}) 
+
+function WrapperComponent({children}) {
+    return <div style = {{
+        display : "flex" , 
+        justifyContent : "center" , 
+        alignItems : "center" , 
+        border : "2px solid lightblue" , 
+        width : "100px"
+    }}> 
+        {children}
+    </div>
+}
