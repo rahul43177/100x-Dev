5f4537f51fd9af6a1d0371d8a1a21b66bcd316e9
Rahul <rahulmishra4317@gmail.com>
2024-12-17 00:21:18 +0530

    useMemo

--- 13-ReactDeepDive/useEffectLearning/src/App.jsx
+++ 13-ReactDeepDive/useEffectLearning/src/App.jsx
@@ -1,49 +1,13 @@
-//https://jsonplaceholder.typicode.com/todos
+import {useState} from 'react'
 
-import {useState,useEffect} from 'react'
-import axios from 'axios'
 export default function App() {
-  const [selectedId , setSelectedId] = useState(1);
-  return (
-    <>
-    <div>
-      <button
-        onClick = {()=> setSelectedId(1)}
-      >1</button>
-      <button
-        onClick={() => setSelectedId(2)}
-      >2</button>
-      <button
-        onClick={() => setSelectedId(3)}
-      >3</button>
-      <button
-        onClick={() => setSelectedId(4)}
-      >4</button>
-    </div>
-      <TodoComponent id = {selectedId}/>
-    </>
-  )
-}
-
-const TodoComponent = ({id}) => {
-  console.log("The id is ->",id)
-  const [todo , setTodo] = useState([]);
-  useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    .then((response) => {
-      console.log("response" , response.data)
-      setTodo(response.data)
-    })
-  },[id])
-  return (    
-    <div>
-      <p>The id - {id}</p>
-      <h4> {todo.title} </h4>
-      <h6
-        style = {{
-          color : todo.completed ? "green" : "red"
-        }}
-      > {todo.completed ? "Ye kaam done hai" : "Ye kaam done nahi hai"} </h6>
-    </div>
-  )
+    const [count , setCount] = useState(0);
+    return (
+        <>
+            <input type="text" placeholder = "enter a number"/>
+            <p>Sum is : {}</p>
+            <button>Counter ({count})</button>
+        </>
+    )
+    
 }
