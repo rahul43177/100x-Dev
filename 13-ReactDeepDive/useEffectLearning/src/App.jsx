b174ddcaf2874a9e6273493222568bde00996c3f
Rahul <rahulmishra4317@gmail.com>
2024-12-17 11:30:02 +0530

    react-react

--- 13-ReactDeepDive/useEffectLearning/src/App.jsx
+++ 13-ReactDeepDive/useEffectLearning/src/App.jsx
@@ -2,11 +2,29 @@ import {useState} from 'react'
 
 export default function App() {
     const [count , setCount] = useState(0);
+    const [text , setText] = useState(0)
+    const [sum , setSum] = useState(0);
+    const findSum = () => {
+        const array = text.split(",")
+        const sumSum = array.reduce((acc , curr) => {
+            return (Number(acc) + Number(curr))
+        })
+        setSum(sumSum);
+    }
     return (
         <>
-            <input type="text" placeholder = "enter a number"/>
-            <p>Sum is : {}</p>
-            <button>Counter ({count})</button>
+            <input 
+                type = "text" 
+                placeholder = "Enter the numbers with comma" 
+                value = {text}
+                onChange = {(e) => setText(e.target.value)}
+                onClick = {findSum}
+            /> 
+            <p>Sum is : {sum}</p>
+            <button
+                onClick = {() => setCount(count+1)}
+            >Counter ({count})</button>
+
         </>
     )
     
