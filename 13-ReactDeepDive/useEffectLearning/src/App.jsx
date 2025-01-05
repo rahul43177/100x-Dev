47cf6052d51d3586da24963c90f569070434f85b
Rahul <rahulmishra4317@gmail.com>
2025-01-05 16:31:22 +0530

    hehehe

--- 13-ReactDeepDive/useEffectLearning/src/App.jsx
+++ 13-ReactDeepDive/useEffectLearning/src/App.jsx
@@ -1,67 +1,20 @@
 import {useState} from 'react'
 
-
-function App() {
+export default function App() {
+    const [counter, setCounter] = useState(0);
+    function updateCounter() {
+        setCounter(counter+1);
+        
+    }
     return (
         <>
-            <div
-                style = {{
-                    display : "flex" , 
-                    // flexDirection : "row"
-                }}
-            >
+            <div>
                 <button
-                    style = {{
-                        padding : 10 , 
-                        margin : 10 , 
-                        border : "2px solid black" ,
-                        borderRadius : "50%" ,
-                        fontFamily : "JetBrains Mono" , 
-                        textDecoration : "underline"
-                    }}
-                >hi There</button>
-                <h3
-                    style = {{
-                        padding : 10 , 
-                        margin : 10 , 
-                        border : "2px solid green" , 
-                        borderRadius : "50px"
-                    }}
-                >Bye there</h3>
-                <a 
-                    href="https://www.google.com" 
-                    target = "_blank"
-                    style = {{
-                        textDecoration : "none" , 
-                        color : "pink" , 
-                        backgroundColor : "black" , 
-                        height : 10 , 
-                        fontFamily : "Gill Sans" , 
-                        padding : 10 , 
-                        margin : 10  ,
-                        margin : "auto" , 
-                        alignContent : "center" , 
-                        
-                    }}
+                    onClick={updateCounter}
                 >
-                    Click here to go to google
-                </a>
-                <a 
-                    href="https://www.instagram.com" 
-                    target = "_blank" 
-                    style = {{
-                        textDecoration : "none" , 
-                        color : "pink" , 
-                        backgroundColor : "black" , 
-                        height : 10 , 
-                        fontFamily : "Gill Sans"
-                    }}
-                >
-                    Click here to go to google
-                </a>
+                    Counter : {counter}
+                </button>
             </div>
         </>
     )
 }
-
-export default App;
