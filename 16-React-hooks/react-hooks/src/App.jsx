1fccc677148d7816eeebe615697c19f1a16fd661
Rahul <rahulmishra4317@gmail.com>
2025-01-15 14:58:02 +0530

    haha

--- 16-React-hooks/react-hooks/src/App.jsx
+++ 16-React-hooks/react-hooks/src/App.jsx
@@ -1,34 +1,25 @@
-import {useState , useMemo} from 'react'
-import LearningUseCallBack from './hooks/useCallback'
+import {memo , useCallback , useState} from 'react'
 
 export default function App() {
-    const [counter , setCounter ] = useState(0)
-    const [inputValue , setInputValue ] = useState(1)
-
-    let count = useMemo(() => {
-        let finalCount = 0;
-        for(let i = 0;i<=inputValue;i++) {
-            finalCount += i;
-        }
-        return finalCount ;
-    }, [inputValue])
-
-    return (
+    const [count , setCount] = useState(0)
+    
+    const inputFunction = useCallback(() => {
+        console.log("Hi There!")
+    })
+        return (
         <div>
-            <LearningUseCallBack/>
-            <input 
-                onChange = {(e) => setInputValue(e.target.value)}
-                type="text" 
-                placeholder='Please enter the number'
-            />
-            <br />
-            <p>The sum - {count}</p>
-            <br />
-            <button
-                onClick={() => setCounter(counter+1)}
-            >
-                Click me to update the counter - {counter}
-            </button>
-        </div>        
+            <ButtonComponent inputFunction = {inputFunction}/>
+            <button 
+                onClick={() => setCount(count+1)}
+            >Click me {count}</button>
+        </div>
     )
 }
+
+const ButtonComponent = memo((inputFunction) => {
+    console.log("Child render!")
+
+    return <div>
+        <button>Button Clicked</button>
+    </div>
+})
