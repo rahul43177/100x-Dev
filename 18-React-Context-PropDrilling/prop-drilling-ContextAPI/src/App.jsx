0759daa2c5a6e7649d4979da6b249a1430ae52ad
Rahul <rahulmishra4317@gmail.com>
2025-02-14 03:54:36 +0530

    I was showing it to muskan

--- 18-React-Context-PropDrilling/prop-drilling-ContextAPI/src/App.jsx
+++ 18-React-Context-PropDrilling/prop-drilling-ContextAPI/src/App.jsx
@@ -1,15 +1,82 @@
-import {BrowserRouter , Routes , Route, useNavigate} from 'react-router-dom'
-import Dashboard from './components/Dashboard'
-import LandingPage from './components/Landing'
+import {useState} from 'react'
 
 export default function App() {
+    const [count , setCount] = useState(0);
     return (
+        <div
+            style = {{
+                display : "flex" , 
+                flexDirection : "column" ,
+                alignItems : "center " , 
+                justifyContent : "center"
+            }}
+        >
+            <h4
+                style = {{
+                    padding : 10 , 
+                    margin : 10 , 
+                    border : "1px solid black" , 
+                    fontFamily : "monospace" , 
+                    width : "fit-content" , 
+                    fontSize : "23px"
+                }}
+            >Passing down the state to the <span style = {{color : "red", textDecoration : "underline"}}>Lowest Commaon Ancestor</span></h4>
 
+            <Counter count = {count}/>
+            <Buttons count = {count} setCount = {setCount}/>
+        </div>
     )
 }
 
+function Counter({count}) {
+    return (
+        <div>
+           <h3>The counter is {count}</h3> 
+        </div>
+    )
+}
 
-function Appbar() {
-    const navigate = useNavigate()
-    
+function Buttons({count , setCount}) {
+    return (
+        <div>
+            <button 
+                onClick={() => setCount(count+1)}
+                style = {{
+                    padding : 10 , 
+                    border : "2px solid green" ,
+                    borderRadius : "50px" , 
+                    margin : 10 , 
+                    width : "fit-content" , 
+                    fontSize : "16px" , 
+                    fontFamily : "cursive"
+                }}
+            >Increment (+)</button>
+            <button 
+                onClick={() => setCount(count-1)}
+                style = {{
+                    padding : 10 , 
+                    border : "2px solid red" ,
+                    borderRadius : "50px" , 
+                    margin : 10 , 
+                    width : "fit-content" , 
+                    fontSize : "16px" , 
+                    fontFamily : "cursive"
+                }}
+            >Decrement (-)</button>
+            <button
+                style = {{
+                    padding : 10 , 
+                    border : "2px solid blue" ,
+                    borderRadius : "50px" ,
+                    margin : 10 ,
+                    width : "fit-content" ,
+                    fontSize : "16px" ,
+                    fontFamily : "cursive"
+                }}
+                onClick={() => setCount(0)}
+            >
+                Reset
+            </button>
+        </div>
+    )
 }
